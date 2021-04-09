e13bb3175cd1ebb80b29a3825b5e375e9488afe5
Pradipta Bora <190050089@iitb.ac.in>
2021-04-09 14:56:48 +0530

    Add files via upload

---
+++ lab8/Testbench.vhd
@@ -0,0 +1,29 @@
+library ieee; 
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity TestBench is 
+end TestBench;
+
+architecture tb of TestBench is
+	signal g,y,r: std_logic_vector(3 downto 0);
+	signal clk, rst: std_logic := '1';
+	
+	component  TrafficLightController is
+		port ( clk, rst : in std_logic;
+			green, yellow, red: out std_logic_vector(3 downto 0));
+	end component;
+begin 
+
+	dut_instance: TrafficLightController
+	port map(clk, rst, g, y, r);
+	
+	process
+	begin 
+	clk <= '0';
+	wait for 2.5 sec;
+	clk <= '1';
+	wait for 2.5 sec;
+	rst <= '0';
+	end process;
+end tb;
